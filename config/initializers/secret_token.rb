74a5c514078653eb0faa074def65de6d893b34da
Savanna R. Baxter <savanna7890@gmail.com>
2014-10-18 11:36:37 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitter::Application.config.secret_key_base = 'f1b0d8de7f2162c5003fec0852330566994eb2f86910fb5aeefa2baebf29f1ae854bef77f56f808a295d5d3ed4d3ebbd430bb145fde3435fb2d17eb2c715286e'
